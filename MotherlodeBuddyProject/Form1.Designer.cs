24c52618c608688b571347e1f7865b2086976b0e
Cezary Ignaszewski <69848161+C-Ign@users.noreply.github.com>
2026-05-28 19:02:38 +0200

    Dodaj pliki projektów.

---
+++ MotherlodeBuddyProject/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace MotherlodeBuddyProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mapPreviewImageContainer = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.mapPreviewImageContainer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mapPreviewImageContainer
+            // 
+            this.mapPreviewImageContainer.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.mapPreviewImageContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapPreviewImageContainer.Location = new System.Drawing.Point(3, 3);
+            this.mapPreviewImageContainer.Name = "mapPreviewImageContainer";
+            this.tableLayoutPanel3.SetRowSpan(this.mapPreviewImageContainer, 2);
+            this.mapPreviewImageContainer.Size = new System.Drawing.Size(628, 537);
+            this.mapPreviewImageContainer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.mapPreviewImageContainer.TabIndex = 0;
+            this.mapPreviewImageContainer.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4",
+            "4"});
+            this.listBox1.Location = new System.Drawing.Point(3, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listBox1.Size = new System.Drawing.Size(412, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AutoSize = true;
+            this.numericUpDown1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 40);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(412, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.AutoSize = true;
+            this.tableLayoutPanel3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.mapPreviewImageContainer, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.tableLayoutPanel4, 1, 1);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(1058, 543);
+            this.tableLayoutPanel3.TabIndex = 10;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.comboBox1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.listBox2, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(637, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.523809F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.523809F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.523809F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.42857F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(418, 265);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Kur Mountains",
+            "Ilmari Desert",
+            "Gazluk"});
+            this.comboBox1.Location = new System.Drawing.Point(3, 28);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(412, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(412, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Motherlode region";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(3, 78);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(412, 184);
+            this.listBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(3, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(412, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Known orientation points";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.AutoSize = true;
+            this.tableLayoutPanel4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Controls.Add(this.numericUpDown1, 0, 1);
+            this.tableLayoutPanel4.Controls.Add(this.listBox1, 0, 2);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(637, 274);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 3;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.42857F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(418, 266);
+            this.tableLayoutPanel4.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(412, 37);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Motherlode amount and distances";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label4.Location = new System.Drawing.Point(0, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "cursor position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(0, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "cursor position";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1058, 543);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Name = "Form1";
+            this.Text = "Motherlode Buddy";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mapPreviewImageContainer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox mapPreviewImageContainer;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MotherlodeBuddyProject/Form1.cs
@@ -0,0 +1,315 @@
+﻿using MotherlodeBuddyProject.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MotherlodeBuddyProject
+{
+    //TODO: zrobić aby koordynaty X były w lustrzanym odbiciu, dzięki czemu położenie punktów na mapie (bez skalowania) będzie poprawne
+    public enum SupportedMaps
+    {
+        AreaKurMountains,
+        AreaDesert1,
+        AreaGazluk
+    }
+    public partial class Form1 : Form
+    {
+        private float mapScaleFactorWidth;
+        private float mapScaleFactorHeight;
+        private float mapZoomFactor = 1;
+        private bool mousePressed;
+        private float offsetX;
+        private float offsetZ;
+        private bool mirroredX;
+        private bool mirroredZ;
+        private SupportedMaps currentArea;
+        private Image currentImageOriginal;
+        private Point lastMouseLocation;
+        private AreaDatabase areaDatabase;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SizeChanged += Form1_SizeChanged;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mapPreviewImageContainer.MouseDown += mapPreviewImage_MouseDown;
+            mapPreviewImageContainer.MouseMove += mapPreviewImage_MouseMove;
+            mapPreviewImageContainer.MouseUp += mapPreviewImage_MouseUp;
+            mapPreviewImageContainer.MouseWheel += mapPreviewImage_MouseWheel;
+            mapPreviewImageContainer.Paint += mapPreviewImage_Paint;
+            string jsonString = Encoding.UTF8.GetString(Resources.LandmarksJSON);
+            areaDatabase = new AreaDatabase();
+            areaDatabase.DeserializeLandmarks();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (mapPreviewImageContainer.Image == null) return;
+
+            RefreshMapVariables();
+            ClampOffset();
+            mapPreviewImageContainer.Invalidate();
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (mapPreviewImageContainer.Image != null)
+            {
+                mapPreviewImageContainer.Image.Dispose();
+                mapPreviewImageContainer.Invalidate();
+            }
+
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    mapPreviewImageContainer.Image = Resources.Map_AreaKurMountains;
+                    currentImageOriginal = mapPreviewImageContainer.Image;
+                    currentArea = SupportedMaps.AreaKurMountains;
+                    mirroredX = true;
+                    break;
+                case 1:
+                    mapPreviewImageContainer.Image = Resources.Map_AreaDesert1;
+                    currentImageOriginal = mapPreviewImageContainer.Image;
+                    currentArea = SupportedMaps.AreaDesert1;
+                    mirroredZ = true;
+                    break;
+                case 2:
+                    mapPreviewImageContainer.Image = Resources.Map_AreaGazluk;
+                    currentImageOriginal = mapPreviewImageContainer.Image;
+                    currentArea = SupportedMaps.AreaGazluk;
+                    mirroredZ = true;
+                    break;
+                default:
+                    break;
+            }
+            RefreshMapVariables();
+            ClampOffset();
+        }
+        private void RefreshMapVariables()
+        {
+            mapScaleFactorWidth = (float)(mapPreviewImageContainer.Width) / (float)(currentImageOriginal.Width);
+
+            mapScaleFactorHeight = (float)(mapPreviewImageContainer.Height) / (float)(currentImageOriginal.Height);
+
+        }
+        private void mapPreviewImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (mapPreviewImageContainer.Image == null) return;
+
+            MouseEventArgs mouse = e;
+            if (mouse.Button == MouseButtons.Left)
+            {
+                mousePressed = true;
+                lastMouseLocation = mouse.Location;
+            }
+        }
+        private void mapPreviewImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousePressed = false;
+        }
+        private void mapPreviewImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mapPreviewImageContainer.Image == null) return;
+            MouseEventArgs mouse = e;
+            label4.Text = "Mouse location X:"+mouse.Location.X.ToString()+" Y:"+mouse.Location.Y.ToString();
+            float xReal;
+            float zReal;
+            if (mirroredX)
+            {
+                xReal = mouse.Location.X / mapScaleFactorWidth;
+                xReal = currentImageOriginal.Width - xReal;
+            }
+            else
+                xReal = mouse.Location.X / mapScaleFactorWidth;
+
+            if (mirroredZ)
+            {
+                zReal = mouse.Location.Y / mapScaleFactorHeight;
+                zReal = currentImageOriginal.Width - zReal;
+            }
+            else
+                zReal = mouse.Location.Y / mapScaleFactorHeight;
+
+
+            label5.Text = "World location X:"+xReal.ToString()+" Z:"+ zReal.ToString();
+
+            if (!mousePressed || mapZoomFactor == mapScaleFactorWidth || mapZoomFactor == mapScaleFactorHeight) return;
+
+
+            int deltaX = mouse.Location.X - lastMouseLocation.X;
+            int deltaY = mouse.Location.Y - lastMouseLocation.Y;
+
+            offsetX += deltaX;
+            offsetZ += deltaY;
+            ClampOffset();
+
+
+            lastMouseLocation = mouse.Location;
+            mapPreviewImageContainer.Invalidate();
+        }
+        private void ClampOffset()
+        {
+            float newWidth = currentImageOriginal.Width * mapZoomFactor;
+            float newHeight = currentImageOriginal.Height * mapZoomFactor;
+
+            float minX = mapPreviewImageContainer.Width - newWidth;
+            float minY = mapPreviewImageContainer.Height - newHeight;
+
+            if (newWidth > mapPreviewImageContainer.Width)
+                offsetX = Math.Max(minX, Math.Min(0, offsetX));
+            else
+                offsetX = (mapPreviewImageContainer.Width - newWidth) / 2;
+
+            if (newHeight > mapPreviewImageContainer.Height)
+                offsetZ = Math.Max(minY, Math.Min(0, offsetZ));
+            else
+                offsetZ = (mapPreviewImageContainer.Height - newHeight) / 2;
+
+
+        }
+        private void mapPreviewImage_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (mapPreviewImageContainer.Image == null) return;
+
+            if (e.Delta > 0)
+                mapZoomFactor += 0.1f;
+
+            if (e.Delta < 0)
+                mapZoomFactor = Math.Max(mapZoomFactor - 0.1f, 1f);
+
+            
+
+            ClampOffset();
+            mapPreviewImageContainer.Invalidate();
+
+        }
+
+        //private void mapPreviewImage_Paint(object sender, PaintEventArgs e)
+        //{
+        //    if (mapPreviewImageContainer.Image == null) return;
+
+
+        //    float newWidth = currentImageOriginal.Width * mapZoomFactor;
+        //    float newHeight = currentImageOriginal.Height * mapZoomFactor;
+
+        //    RectangleF destRect = new RectangleF(offsetX, offsetZ, newWidth, newHeight);
+        //    RectangleF srcRect = new RectangleF(0, 0, currentImageOriginal.Width,   currentImageOriginal.Height);
+
+
+        //    e.Graphics.DrawImage(currentImageOriginal, destRect, srcRect, GraphicsUnit.Pixel);
+        //    Brush pointBrush = new SolidBrush(Color.Red);
+
+        //    foreach (var landmark in areaDatabase.allAreasData[currentArea.ToString()].Landmarks)
+        //    {
+        //        PointF originalPixel = GetWorldPixelCoordinates(landmark.WorldXCoordinate, landmark.WorldZCoordinate);
+
+        //        PointF screenPixel = GetScreenCoordinates(originalPixel, mapZoomFactor, offsetX, offsetZ);
+
+        //        e.Graphics.FillEllipse(pointBrush, originalPixel.X - 5, originalPixel.Y - 5, 10, 10);
+        //    }
+
+        //    pointBrush.Dispose();
+        //}
+        private void mapPreviewImage_Paint(object sender, PaintEventArgs e)
+        {
+            if (mapPreviewImageContainer.Image == null) return;
+            RectangleF srcRect = new RectangleF(0, 0, currentImageOriginal.Width * mapScaleFactorWidth, currentImageOriginal.Height * mapScaleFactorHeight);
+
+
+            e.Graphics.DrawImage(currentImageOriginal, srcRect);
+            Brush pointBrush = new SolidBrush(Color.Red);
+
+            foreach (var landmark in areaDatabase.allAreasData[currentArea.ToString()].Landmarks)
+            {
+                PointF originalPixel = GetWorldPixelCoordinates(landmark.WorldXCoordinate, landmark.WorldZCoordinate);
+
+                //PointF screenPixel = GetScreenCoordinates(originalPixel, mapZoomFactor, offsetX, offsetZ);
+
+                e.Graphics.FillEllipse(pointBrush, originalPixel.X - 5, originalPixel.Y - 5, 10, 10);
+            }
+
+            pointBrush.Dispose();
+        }
+        public PointF GetWorldPixelCoordinates(float worldX, float worldZ)
+        {
+            float pixelX = worldX * ((mapPreviewImageContainer.Width) / (currentImageOriginal.Width));
+            float pixelZ = worldZ * ((mapPreviewImageContainer.Height) / (currentImageOriginal.Height));
+
+            return new PointF(pixelX, pixelZ);
+        }
+        public PointF GetScreenCoordinates(PointF originalPixel, float zoomFactor, float viewOffsetX, float viewOffsetY)
+        {
+            float screenX = (originalPixel.X * zoomFactor) + viewOffsetX;
+            float screenY = (originalPixel.Y * zoomFactor) + viewOffsetY;
+
+            return new PointF(screenX, screenY);
+        }
+        private void placeKnownLandmarks(string areaName, PaintEventArgs e)
+        {
+            Pen whitePen = new Pen(Brushes.White);
+            
+        }
+    }
+    public class Landmark 
+    {
+        [JsonPropertyName("Name")]
+        public string LandmarkName { get; set;  }
+        [JsonPropertyName("X")]
+        public float WorldXCoordinate { get; set; }
+        [JsonPropertyName("Z")]
+        public float WorldZCoordinate { get; set; }
+    }
+    public class Anchor
+    {
+        [JsonPropertyName("X")]
+        public float WorldXCoordinate { get; set; }
+        [JsonPropertyName("Z")]
+        public float WorldZCoordinate { get; set; }
+        [JsonPropertyName("ImageX")]
+        public int ImageXCoordinate { get; set; }
+        [JsonPropertyName("ImageZ")]
+        public int ImageZCoordinate { get; set; }
+    }
+    public class AreaData
+    {
+        public List<Landmark> Landmarks { get; set; }
+        public List<Anchor> Anchors { get; set; }
+    }
+    public class AreaDatabase : ISerializable
+    {
+        public Dictionary<string, AreaData> allAreasData;
+
+        public void DeserializeLandmarks()
+        {
+            allAreasData = JsonSerializer.Deserialize<Dictionary<string, AreaData>>(Resources.LandmarksJSON);
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            return;
+        }
+    }
+
+}
